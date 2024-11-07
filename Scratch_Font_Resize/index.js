e1f41e040aac5a7d426142fde3210c35cac4d65c
SamSP <113569096+AsakarP@users.noreply.github.com>
2024-11-07 17:44:18 +0700

    Update kepada font resize

--- Scratch_Font_Resize/index.js
+++ Scratch_Font_Resize/index.js
@@ -8,8 +8,21 @@ import { ImageLib } from "./lib.js";
 
 const lib = new ImageLib('my_canvas')
 
+const lib2 = new ImageLib('canvasHitam')
+
+const lib3 = new ImageLib('canvasPutih')
+
 let canvas = lib.canvas_handler;
 
+let canvasHitam = lib2.canvas_handler;
+
+let canvasPutih = lib3.canvas_handler;
+
+let color = {r:0, g:0, b:0};
+
+let warnaHitam = true;
+
+// Untuk Menampung Koordinat titik
 let titik = [];
 
 // Fungsi Mengambil Input Text dan menempelkan pada p
@@ -23,48 +36,90 @@ window.tempelText = tempelText;
 // Saat klik di tahan maka akan menggambar
 canvas.addEventListener('mousedown', function (ev) {
     var rect = canvas.getBoundingClientRect();
-
     var x = ev.clientX - rect.left;
     var y = ev.clientY - rect.top;
-    console.log(x + " " + y);
-
-    titik.push({x: x, y: y});
-
-    lib.create_dot(x, y, 0);
+    lib.create_dot(x, y, color);
+    lib.create_dot(x+1, y, color);
+    lib.create_dot(x-1, y, color);
+    lib.create_dot(x, y+1, color);
+    lib.create_dot(x, y-1, color);
+    lib.create_dot(x+1, y+1, color);
+    lib.create_dot(x-1, y-1, color);
+    lib.create_dot(x+1, y-1, color);
+    lib.create_dot(x-1, y+1, color);
     lib.draw();
-
-    resizeFont();
-
+    titik.push({x: x, y: y});
     canvas.addEventListener('mousemove', onMouseMove);
 });
 
 // Saat klik di lepas maka akan berhenti menggambar
 canvas.addEventListener('mouseup', function () {
+    resizeFont();
     canvas.removeEventListener('mousemove', onMouseMove);
-    console.log(totalScratch())
+    console.log(titik.length)
 })
 
 // Fungsi menggambar saat klik di tahan
 function onMouseMove(ev) {
     var rect = canvas.getBoundingClientRect();
+    var x = ev.clientX - rect.left;
+    var y = ev.clientY - rect.top;
+    lib.create_dot(x, y, color);
+    lib.create_dot(x+1, y, color);
+    lib.create_dot(x-1, y, color);
+    lib.create_dot(x, y+1, color);
+    lib.create_dot(x, y-1, color);
+    lib.create_dot(x+1, y+1, color);
+    lib.create_dot(x-1, y-1, color);
+    lib.create_dot(x+1, y-1, color);
+    lib.create_dot(x-1, y+1, color);
+    lib.draw();
+    if (warnaHitam == true) {
+        titik.push({x: x, y: y});
+    }
+    else {
+        titik.pop();
+    }
+}
 
+// Fungsi Mengubah Ukuran Font
+function resizeFont() {
+    let totalTitik = titik.length;
+    if (warnaHitam == true) {
+        textIni.style.fontSize = `${20 + 0.05 * totalTitik}px`
+    }
+    else {
+        textIni.style.fontSize = `${20 - 0.05 * totalTitik}px`
+    }
+}
+
+// Canvas Hitam
+for (let i=0; i<50; i++) {
+    for (let j=0; j<50; j++) {
+        lib2.create_dot(0+i, 0+j, 0)
+    }
+}
+
+canvasHitam.addEventListener('click', function (ev) {
+    var rect = canvasHitam.getBoundingClientRect();
     var x = ev.clientX - rect.left;
     var y = ev.clientY - rect.top;
     console.log(x + " " + y);
-    titik.push({x: x, y: y});
-    lib.create_dot(x, y, 0);
-    lib.draw();
+    color = {r:0,g:0,b:0}
+    warnaHitam = true
+});
 
-    resizeFont();
-}
-
-function totalScratch() {
-    return titik.length;
-}
-
-function resizeFont() {
-    let totalTitik = titik.length;
-    textIni.style.fontSize = `${10 + 0.05*totalTitik}px`
-}
+// Canvas Putih
+canvasPutih.addEventListener('click', function (ev) {
+    var rect = canvasHitam.getBoundingClientRect();
+    var x = ev.clientX - rect.left;
+    var y = ev.clientY - rect.top;
+    console.log(x + " " + y);
+    color = {r:255,g:255,b:255}
+    warnaHitam = false
+});
 
 
+// Penghapus
+
+lib2.draw();
